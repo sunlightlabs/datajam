fa1b4b9929703e2b92fedc10ceadc57b9e685657
Luigi Montanez <luigi.montanez@gmail.com>
2011-06-09 11:12:49 -0400

    Generate a slug for events. [#12271641]

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Slug
 
   field :name,           type: String
   field :scheduled_at,   type: Time
@@ -8,8 +9,14 @@ class Event
   field :template_data,  type: Hash
   field :activities,     type: Hash
 
+  slug :name
+
   belongs_to :template
   has_and_belongs_to_many :users
 
+  # Mongoid::Slug changes this to `self.slug`. No thanks.
+  def to_param
+    self.id.to_s
+  end
 end
 
