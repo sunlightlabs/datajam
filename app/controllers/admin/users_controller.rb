31a3dd3f9baa667b5f996a3d021d2b6b2af71a85
Luigi Montanez <luigi.montanez@gmail.com>
2011-06-22 11:45:23 -0400

    Implement user creation and deletion [#12271589]

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,51 @@
+class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "User saved."
+      redirect_to admin_users_path
+    else
+      flash[:error] = "There was a problem saving the user."
+      redirect_to admin_users_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    params[:user].delete(:password) if params[:user][:password].blank?
+    params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User updated."
+      redirect_to admin_users_path
+    else
+      flash[:error] = "There was a problem saving the user."
+      redirect_to admin_users_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  field :name,        type: String
+  field :affiliation, type: String
+  field :url,         type: String
 end
