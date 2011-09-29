77ddd9c7029e9a8e5ab44dad3a6f5edc2df996e3
Luigi Montanez <luigi.montanez@gmail.com>
2011-09-29 18:17:05 -0400

    Upgrade to Rails 3.1
    No asset pipeline support (yet). [#18985409]

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,5 +3,5 @@ class Asset
   include Mongoid::Timestamps
 
   field :name, type: String
-  mount_uploader :asset, AssetUploader
+  mount_uploader :asset, AssetUploader, mount_on: :asset_filename
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Datajam::Application.configure do
   config.cache_classes                          = false
   config.whiny_nils                             = true
   config.consider_all_requests_local            = true
-  config.action_view.debug_rjs                  = true
+#  config.action_view.debug_rjs                  = true
   config.action_controller.perform_caching      = false
   config.action_mailer.default_url_options      = { :host => 'localhost:3000' }
   config.action_mailer.raise_delivery_errors    = false
