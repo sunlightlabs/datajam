42163c6520de3df9b564d08bc141e3e52db6fd3f
Dan Drinkard <dan.drinkard@gmail.com>
2011-11-21 20:21:57 -0500

    starting on plugin settings

---
+++ app/models/setting.rb
@@ -0,0 +1,14 @@
+class Setting
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :namespace, type: String
+  field :name, type: String
+  field :value, type: String
+
+  index [[:namespace, Mongo::ASCENDING], [:name, Mongo::ASCENDING]], :unique: true
+
+  validates_presence_of :namespace, :name
+  validates_uniqueness_of :name, :scope => :namespace
+
+end
