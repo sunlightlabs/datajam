5e61183fa1a8c3cde30d1cd188e6dedaefb190db
Dan Drinkard <dan.drinkard@gmail.com>
2011-11-29 16:12:30 -0500

    settings interface

---
+++ config/initializers/settings.rb
@@ -0,0 +1,41 @@
+module Datajam
+  module Settings
+
+    def self.[](namespace)
+      ns = namespace.to_sym
+      @@settings ||= {}
+      @@settings[ns] ||= {}
+      unless @@settings[ns].any?
+        settings = Setting.where(:namespace => namespace.to_s)
+        settings.each do |setting|
+          @@settings[ns][setting.name.to_sym] ||= setting.value
+        end
+      end
+      @@settings[ns]
+    end
+
+    def self.get(key, options)
+      @@settings ||= {}
+      default = Proc.new { return options[:default] rescue nil }
+
+      key = key.to_s
+      unless options.is_a? Hash
+        options = {:namespace => options.to_s}
+      end
+      default.call unless options.include?(:namespace)
+
+      @@settings[options[:namespace].to_sym] ||= {}
+      begin
+        @@settings[options[:namespace].to_sym][key.to_sym] ||= Setting.where(:namespace => options[:namespace], :name => key).first.value
+      rescue
+        default.call
+      end
+    end
+
+    def self.flush(namespace)
+      @@settings[namespace.to_sym] = {}
+      true
+    end
+  end
+
+end
