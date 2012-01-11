9326ff4778eff7227282bba854a7354424595e8e
Dan Drinkard <dan.drinkard@gmail.com>
2012-01-11 18:08:21 -0500

    event updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -39,7 +39,7 @@ class Event
   end
 
   def script_id
-    '<script type="text/javascript">window.Datajam = window.Datajam || {}; Datajam.eventId = "' + self.id.to_s + '";</script>'
+    '<script type="text/javascript">window.Datajam || (Datajam = {}); Datajam.eventId = "' + self.id.to_s + '";</script>'
   end
 
   # Render the HTML for an event page
