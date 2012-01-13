77a9cf3008d95c2fa1f5c69b6b8102ca5f5336ce
Dan Drinkard <dan.drinkard@gmail.com>
2012-01-13 11:47:01 -0500

    skip varnish if redis-cache is in play

--- app/middlewares/redis_cache.rb
+++ app/middlewares/redis_cache.rb
@@ -44,6 +44,7 @@ class RedisCache
 
     [200, {'Content-Type'   => mimetype,
            'Content-Length' => cached_content.length.to_s
+           'Cache-Control' => 'no-cache'
           }, [cached_content]]
   end
 
