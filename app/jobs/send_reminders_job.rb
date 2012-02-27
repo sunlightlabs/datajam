9dded4932391896037df9764ff799b0431d4fce3
Dan Drinkard <dan.drinkard@gmail.com>
2012-02-27 12:02:17 -0500

    increase reminder time window, ensure not sent twice

--- app/jobs/send_reminders_job.rb
+++ app/jobs/send_reminders_job.rb
@@ -1,12 +1,15 @@
 module SendRemindersJob
-  NOTIFY_TIME_BEFORE_EVENT = Time.now + 1.hour
+  # Task runs every hour, ensure at least 30 minutes lead time
+  NOTIFY_TIME_BEFORE_EVENT = Time.now + 90.minutes
 
   def self.perform
     Event.all.where(status: 'Upcoming', :scheduled_at.gt => Time.now).and(:scheduled_at.lt => NOTIFY_TIME_BEFORE_EVENT).each do |event|
       puts "Notifications for: " + event.name if event.reminders.any?
       event.reminders.each do |reminder|
-        puts "  -> Sending event reminder to: " + reminder.email
-        NotificationMailer.event_reminder(event, reminder).deliver
+        unless reminder.sent?
+          puts "  -> Sending event reminder to: " + reminder.email
+          NotificationMailer.event_reminder(event, reminder).deliver
+        end
       end
     end
   end
--- lib/tasks/notifications.rake
+++ lib/tasks/notifications.rake
@@ -1,5 +1,5 @@
 namespace :notifications do
-  desc "test"
+  desc "Send email reminders to the notification queue for the next upcoming event"
   task :send_reminders => :environment do
     SendRemindersJob.perform
   end
