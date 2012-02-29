7f9be765d905eec9e7b80cca20686e971fda0158
Dan Drinkard <dan.drinkard@gmail.com>
2012-02-29 15:34:35 -0500

    make jquery available sooner in public-facing pages

--- config/initializers/01_static_assets.rb
+++ config/initializers/01_static_assets.rb
@@ -1,6 +1,7 @@
 HEAD_ASSETS = <<-HEAD.strip_heredoc
 <link rel="stylesheet" href="/datajam.css" type="text/css" media="screen, projection">
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.0.6/modernizr.min.js" type="text/javascript"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript">
   window.curl || (curl = {
     baseUrl: '/javascripts',
@@ -12,7 +13,6 @@ HEAD
 
 BODY_ASSETS = <<-BODY.strip_heredoc
 <script src="http://cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2.js" type="text/javascript"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7/jquery.min.js" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.2.1/underscore-min.js" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.5.3/backbone-min.js" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0.beta2/handlebars.min.js" type="text/javascript"></script>
