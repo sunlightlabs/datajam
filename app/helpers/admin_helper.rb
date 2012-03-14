88c85ff45a1e1e8f8efad1c762788996af9e591e
Bruno Aguirre <yo@brunoaguirre.com>
2012-03-14 16:27:51 -0300

    Globally Nice Delete Buttons [#14]

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,10 @@ module AdminHelper
   def table_for(collection, headers, &row)
     render "shared/table", headers: Array(headers), collection: Array(collection), generator: row
   end
+
+  def delete_button(path, options={})
+    text = options.fetch(:label, "Delete")
+    class_name = "btn btn-danger btn-small #{options.delete(:class)}".strip
+    button_to text, path, options.reverse_merge(method: :delete, class: class_name)
+  end
 end
