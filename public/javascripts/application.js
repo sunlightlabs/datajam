ff0b1466e930d7b16f40f1f10331e81c7907b796
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2012-03-14 23:23:48 -0300

    Fix the issue with the ACE Editor
    Also, remove all those #ace-editor divs, and create a container
    automatically for all textarea.datajamTemplate [#14]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,20 @@
 window.onload = function() {
+  function createEditorContainer(element) {
+    var container = $("<div/>");
+    container.attr("id", "editor-" + element.attr("id"));
+    container.addClass("ace-editor");
+    element.after(container)
+    return container;
+  }
+
   $("textarea.datajamTemplate").each(function() {
     var $el = $(this),
-        editor = ace.edit($el.data("editor") || "ace-editor"),
+        container = createEditorContainer($el),
+        editor = ace.edit(container.attr("id")),
         HtmlMode = require("ace/mode/html").Mode,
         editorSession = editor.getSession();
+
+    $el.css({ display: "none" });
 
     editorSession.setMode(new HtmlMode());
     editorSession.setValue($el.val());
