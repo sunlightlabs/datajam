a56979e12d99eff6496211d13239fb350a06d874
Dan Drinkard <dan.drinkard@gmail.com>
2012-09-26 13:02:07 -0400

    access setting directly

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,7 @@ class NotificationMailer < ActionMailer::Base
 
   def event_reminder(event, reminder)
     @event = event
-    @settings = Setting.where(namespace: 'datajam')
+    @reminder_message = Datajam::Settings[:datajam][:reminder_email_message]
     mail(:to => reminder.email, :subject => "#{event.name} will start shortly!")
     reminder.sent!
   end
