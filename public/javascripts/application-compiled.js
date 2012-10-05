c4ea12747c7dc8eb583f2164dd1d8f00b7d190db
Dan Drinkard <dan.drinkard@gmail.com>
2012-10-05 15:05:32 -0400

    Don't try to bootstrap when there's not an eventId

--- public/javascripts/application-compiled.js
+++ public/javascripts/application-compiled.js
@@ -576,6 +576,7 @@
 (function(define, require, $, window, undefined){
 
   require(['datajam/init', 'datajam/views/event'], function(){
+    if(! Datajam.eventId) return false;
     var el = $('body').prepend('<div class="datajam-event" />').find('.datajam-event');
     Datajam.event = new Datajam.views.Event({ el: el });
   });
