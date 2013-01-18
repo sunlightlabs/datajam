85e89caedcfc7a2ba75b311484b1f04709a5e828
Dan Drinkard <dan.drinkard@gmail.com>
2013-01-18 16:25:46 -0500

    map seeds to new datacard fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,7 +46,7 @@ event_template = EventTemplate.find_or_create_by(
   </div>
   EOT
 )
-if event_template.errors.count
+if event_template.errors.any?
   event_template = EventTemplate.where(name: 'Default').first
 end
 
@@ -68,11 +68,13 @@ end
 
 data_card_1 = DataCard.find_or_create_by(
   title: 'Data Card 1',
-  body: "<h3>This is a data card</h3>\r\n<p>Data cards can be raw html, or csv parsed into a template.</p>\r\n<p>\r\n  This provides the flexibility to embed just about anything. \r\n  To change the data card, ensure you're logged in and then select\r\n  'Data Card' from the On Air toolbar.   \r\n</p>",
+  display_type: :html,
+  html: "<h3>This is a data card</h3>\r\n<p>Data cards can be raw html, or csv parsed into a template.</p>\r\n<p>\r\n  This provides the flexibility to embed just about anything. \r\n  To change the data card, ensure you're logged in and then select\r\n  'Data Card' from the On Air toolbar.   \r\n</p>",
 )
 data_card_2 = DataCard.find_or_create_by(
   title: 'Data Card 2',
-  body: "<h3>Try creating your own!</h3><p>To create your own data cards, click 'Data Cards' in the admin site.</p>"
+  display_type: :html,
+  html: "<h3>Try creating your own!</h3><p>To create your own data cards, click 'Data Cards' in the admin site.</p>"
 )
 dca = event.content_areas.last
 dca.update_attributes!({current_card_id: data_card_1._id, html: dca.render})
